df74682e9ec79a754144ebc041238c6913255a55
Andrew Throener <athroener@mystartupsolutions.com>
2017-12-05 11:42:56 -0600

    Comile to lib/es6 and lib/cjs and add package.json properties
    Review
    
    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,25 @@
-export default {
+export default [{
+    input: 'src/index.js',
+    name: 'sfCommerceCloudClient',
+    output: {
+        file: 'dist/commercecloud-ocapi-client.umd.js',
+        format: 'umd'
+    },
+    external: ['superagent', 'querystring'],
+},
+{
     input: 'src/index.js',
     output: {
         file: 'lib/index.js',
+        format: 'cjs'
+    },
+    external: ['superagent', 'querystring'],
+},
+{
+    input: 'src/index.js',
+    output: {
+        file: 'es/index.js',
         format: 'es'
     },
     external: ['superagent', 'querystring'],
-}
+}]
