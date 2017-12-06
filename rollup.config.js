31e962df29c9af7e223563d669bcd28c0d6d7af6
Kevin He <khe@mobify.me>
2017-12-06 12:08:09 -0800

    fix rollup umd output

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,45 @@
+
+import builtins from 'rollup-plugin-node-builtins'
+import replace from 'rollup-plugin-re'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json'
+
 export default [{
     input: 'src/index.js',
-    name: 'sfCommerceCloudClient',
+    name: 'ocapi-client',
     output: {
-        file: 'dist/commercecloud-ocapi-client.umd.js',
+        file: 'dist/commercecloud-ocapi-client.js',
         format: 'umd'
     },
-    external: ['superagent', 'querystring'],
+    plugins: [
+        json(),
+        /*
+         * NOTE: Do replace before commonjs
+         * This is a hack for rollup
+         * In current rollupJS version, it does not handle reassigned require function
+         * in certain packages that uses commonjs helper
+         * We need to manually remove the hijacked require function
+         * For more info: https://github.com/rollup/rollup-plugin-commonjs/issues/166#issuecomment-328853157
+         */
+        replace({
+            patterns: [{
+                match: /formidable(\/|\\)lib/,
+                test: 'if (global.GENTLY) require = GENTLY.hijack(require);',
+                replace: '',
+            }]
+        }),
+        commonjs({
+            namedExports: {
+                superagent: ['superagent'],
+            },
+        }),
+        resolve({
+            preferBuiltins: false,
+            querystring: true,
+        }),
+        builtins(),
+    ]
 },
 {
     input: 'src/index.js',
